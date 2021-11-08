5b752db008007f1eeff85bd9a803891202ec4b06
pixlblade <79928968+pixlblade@users.noreply.github.com>
2021-11-08 18:25:01 -0500

    Clean up

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Storange
 A minecraft mod that makes storing items easier, and much more!
+## What is storange?
+Storange is a minecraft mod, similar to AE2 and Refined Storage that lets you store items in a digital interface, craft using those items, and autocraft items through the system. But there will be so much more!
+
+*Note: Storange is Fabric only.*
--- src/main/kotlin/net/pixlblade/storange/ItemReg.kt
+++ src/main/kotlin/net/pixlblade/storange/ItemReg.kt
@@ -5,14 +5,17 @@ import net.minecraft.item.Item
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
-// Object for easily registering items
+// Object for easily registering items that have no special properties/are crafting items
+// I coooould use a class for this but there's only gonna be one instance at init so what's the point lol
 object ItemReg {
     private val ItemList = mutableListOf<String>()
     private val namespace = "storange"
+    // Create an item instance and add the name to the itemlist
     fun make(name: String): Item {
         ItemList.add(name)
         return Item(FabricItemSettings().group(Storange.StorangeGroup))
     }
+    // Register all items created using their names
     fun register(vararg items: Item) {
         var i = 0
         for(item in items) {
