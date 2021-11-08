89c18333b8af5f33979b502e0f5642ac39051975
pixlblade <79928968+pixlblade@users.noreply.github.com>
2021-11-08 18:03:34 -0500

    Finish adding all items back in

--- src/main/kotlin/net/pixlblade/storange/Storange.kt
+++ src/main/kotlin/net/pixlblade/storange/Storange.kt
@@ -6,10 +6,7 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Block
 import net.minecraft.block.Material
-import net.minecraft.item.BlockItem
-import net.minecraft.item.Item
-import net.minecraft.item.ItemGroup
-import net.minecraft.item.ItemStack
+import net.minecraft.item.*
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
@@ -19,17 +16,22 @@ object Storange : ModInitializer {
     val StorangeGroup: ItemGroup = FabricItemGroupBuilder.build(Identifier("storange", "general")) { ItemStack(Orange) }
 
     // Items
-    val Orange = Item(FabricItemSettings().group(StorangeGroup))
+    val Orange = Item(FabricItemSettings().group(StorangeGroup).food(FoodComponent.Builder().hunger(4).build()))
     val Hammer = ItemReg.make("hammer")
     val IronPlate = ItemReg.make("iron_plate")
-
+    val MiniSiliconWafer = ItemReg.make("mini_silicon_wafer")
+    val Processor = ItemReg.make("processor")
+    val ProcessorChip = ItemReg.make("processor_chip")
+    val SiliconWafer = ItemReg.make("silicon_wafer")
+    val Transistor = ItemReg.make("transistor")
 
     // Blocks
     val MachineBlock = Block(FabricBlockSettings.of(Material.METAL).strength(5.0f))
+
     override fun onInitialize() {
         // Items
         Registry.register(Registry.ITEM, Identifier("storange", "orange"), Orange)
-        ItemReg.register(Hammer, IronPlate)
+        ItemReg.register(Hammer, IronPlate, MiniSiliconWafer, Processor, ProcessorChip, SiliconWafer, Transistor)
 
         // Blocks
         Registry.register(Registry.BLOCK, Identifier("storange", "machine_block"), MachineBlock)
